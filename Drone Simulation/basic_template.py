d2075a797b661ff90d2c73007081a29214937352
AndyWangLYN <90009289+AndyWangLYN@users.noreply.github.com>
2022-12-13 21:44:53 -0600

    Add files via upload

--- Drone Simulation/basic_template.py
+++ Drone Simulation/basic_template.py
@@ -1,55 +1,49 @@
+# Import necessary modules from the DroneKit library
 from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+
+# Import other necessary modules
 import time
 import socket
 import exceptions
 import math
 import argparse
- 
+
+#####FUNCTIONS#####
+
+# Function to connect to the drone
 def connectMyCopter():
- 
+
+    # Use argparse to parse command-line arguments
     parser = argparse.ArgumentParser(description='commands')
     parser.add_argument('--connect')
     args = parser.parse_args()
- 
     connection_string = args.connect
-     
+
+    # If no connection string was provided, start the default SITL simulator
     if not connection_string:
         import dronekit_sitl
         sitl = dronekit_sitl.start_default()
         connection_string = sitl.connection_string()
- 
+
+    # Connect to the drone and return the vehicle object
     vehicle = connect(connection_string, wait_ready=True)
- 
     return vehicle
- 
-def arm_and_takeoff(targetHeight):
-    while vehicle.is_armable!=True:
-        print("Waiting for vehicle to become aramable")
-        time.sleep(1)
-    print("Vehicle is now armable")
- 
-    vehicle.mode = VehicleMode("GUIDED")
- 
-    while vehicle.mode!="GUIDED":
-        print("Waiting for drone to enter GUIDED flight mode")
-        time.sleep(1)
-    print("Vehicle now in GUIDED MODE. Have fun!!")
- 
-    vehicle.armed = True
-    while vehicle.armed==False:
-        print("Waiting for drone to become armed")
-        time.sleep(1)
-    print("Look out! Virtual props are spinning!")
- 
-    vehicle.simple_takeoff(targetHeight) ##meters
- 
-    while True:
-        print("Current Altitude:", vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt>=.95*targetHeight:
-            break
-        time.sleep(1)
-    print("Target altitude reached!!")
-    return None
- 
+
+#####MAIN EXECUTABLE#####
+
+# Connect to the drone and retrieve the vehicle object
 vehicle = connectMyCopter()
-arm_and_takeoff(10)
+
+# Wait until the autopilot is ready and then print its version
+vehicle.wait_ready('autopilot_version')
+print('AutoPilot version: %s' % vehicle.version)
+
+# Print the drone's current position, attitude, velocity, and last heartbeat
+print('Position: %s' % vehicle.location.global_relative_frame)
+print('Attitude: %s' % vehicle.attitude)
+print('Velocity: %s' % vehicle.velocity)
+print('Last heartbeat: %s' % vehicle.last_heartbit)
+
+# Print whether the vehicle is armable and its current flight mode
+print('Is the vehicle good to arm: %s' % vehicle.is_armable)
+print('Current flight mode: %s' % vehicle.mode.name)
--- Drone Simulation/location_based_movement.py
+++ Drone Simulation/location_based_movement.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
 import time
 import socket
@@ -6,42 +7,41 @@ import math
 import argparse
  
 def connectMyCopter():
- 
+    # Parse command line arguments to get connection string
     parser = argparse.ArgumentParser(description='commands')
     parser.add_argument('--connect')
     args = parser.parse_args()
  
     connection_string = args.connect
      
+    # If no connection string was provided, start the Sitl simulation
     if not connection_string:
         import dronekit_sitl
         sitl = dronekit_sitl.start_default()
         connection_string = sitl.connection_string()
  
+    # Connect to the drone
     vehicle = connect(connection_string, wait_ready=True)
  
     return vehicle
  
 def arm_and_takeoff(targetHeight):
+    # Wait for the drone to become armable
     while vehicle.is_armable!=True:
         print("Waiting for vehicle to become aramable")
         time.sleep(1)
     print("Vehicle is now armable")
  
+    # Set the drone's flight mode to "GUIDED"
     vehicle.mode = VehicleMode("GUIDED")
  
+    # Wait for the drone to enter "GUIDED" mode
     while vehicle.mode!="GUIDED":
-        print("Waiting for drone to enter GUIDED flight mode")
-        time.sleep(1)
-    print("Vehicle now in GUIDED MODE. Have fun!!")
- 
-    vehicle.armed = True
-    while vehicle.armed==False:
         print("Waiting for drone to become armed")
         time.sleep(1)
     print("Look out! Virtual props are spinning!")
  
-    vehicle.simple_takeoff(targetHeight) ##meters
+    vehicle.simple_takeoff(targetHeight) ## in meters
  
     while True:
         print("Current Altitude:", vehicle.location.global_relative_frame.alt)
@@ -51,13 +51,14 @@ def arm_and_takeoff(targetHeight):
     print("Target altitude reached!!")
     return None
  
- 
+#  calculate the distance in meters and return
 def get_distance_meters(targetLocation, currentLocation):
     dLat = targetLocation.lat - currentLocation.lat
     dLon = targetLocation.lon - currentLocation.lon
  
     return math.sqrt((dLon*dLon)+(dLat*dLat))*1.113195e5
  
+#  fly the drone to the location specified
 def goto(targetLocation):
     distanceToTargetLocation = get_distance_meters(targetLocation, vehicle.location.global_relative_frame)
  
@@ -65,6 +66,7 @@ def goto(targetLocation):
  
     while vehicle.mode.name == "GUIDED":
         currentDistance = get_distance_meters(targetLocation,vehicle.location.global_relative_frame)
+        # when the drone is close to the location, change its state
         if currentDistance < distanceToTargetLocation*.01:
             print("Reached target waypoint")
             time.sleep(2)
@@ -72,12 +74,14 @@ def goto(targetLocation):
         time.sleep(1)
     return None
  
+#  define the location
 wp1 = LocationGlobalRelative(29.720273, -95.399829, 10)
  
 vehicle = connectMyCopter()
 arm_and_takeoff(10)
  
-goto(wp1)
+#  fly the drone
+goto(wp1) 
  
 vehicle.mode = VehicleMode("LAND")
 while vehicle.mode != 'LAND'
